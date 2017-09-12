1ab5946a4af33dea2551284856c158174abc6dbd
caofei <1594717129@qq.com>
2017-09-12 14:38:59 +0800

    init commit

---
+++ Example/Pods/Target Support Files/Pods-ZZCoreManager_Tests/Pods-ZZCoreManager_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZZCoreManager_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ZZCoreManager_Tests
+@end
---
+++ Example/Pods/Target Support Files/ZZCoreManager/ZZCoreManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZZCoreManager : NSObject
+@end
+@implementation PodsDummy_ZZCoreManager
+@end
